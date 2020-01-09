bac1c57573ebd04cf71aba3dcfda6721f45fc766
travnj01 <jan.travnicek@dixonscarphone.com>
2020-01-09 09:13:05 +0100

    finish task of the day_01

--- day_01/index.js
+++ day_01/index.js
@@ -4,9 +4,17 @@ export function getFuel(mass) {
   return Math.floor(mass / 3) - 2
 }
 
-export function sumFuelRequirements(inputFileName) {
+export function getFuel2(mass) {
+  const tempResult = getFuel(mass)
+  if (tempResult <= 0) {
+    return 0
+  }
+  return tempResult + getFuel2(tempResult)
+}
+
+export function sumFuelRequirements(inputFileName, callback) {
   return readFileSync(inputFileName, 'utf8')
     .split('\n')
     .map((value) => parseInt(value, 10))
-    .reduce((acc, val) => acc + getFuel(val), 0)
+    .reduce((acc, val) => acc + callback(val), 0)
 }
--- day_01/index.test.js
+++ day_01/index.test.js
@@ -1,4 +1,4 @@
-import { getFuel, sumFuelRequirements } from './index'
+import { getFuel, getFuel2, sumFuelRequirements } from './index'
 
 describe('given examples', () => {
   test('example 1', () => {
@@ -17,12 +17,32 @@ describe('given examples', () => {
 
 describe('first part of task', () => {
   test('test input', () => {
-    expect(sumFuelRequirements('./day_01/input_test.txt')).toBe(34241)
+    expect(sumFuelRequirements('./day_01/input_test.txt', getFuel)).toBe(34241)
+  })
+
+  test('given input', () => {
+    expect(sumFuelRequirements('./day_01/input.txt', getFuel)).toBe(3254441)
   })
 })
 
-describe('first part of task', () => {
+describe('second part of task', () => {
+  test('example 1', () => {
+    expect(getFuel2(14)).toBe(2)
+  })
+  test('example 2', () => {
+    expect(getFuel2(1969)).toBe(966)
+  })
+  test('example 3', () => {
+    expect(getFuel2(100756)).toBe(50346)
+  })
+})
+
+describe('second part - given input', () => {
+  test('test input', () => {
+    expect(sumFuelRequirements('./day_01/input_test.txt', getFuel2)).toBe(51316)
+  })
+
   test('given input', () => {
-    expect(sumFuelRequirements('./day_01/input.txt')).toBe(1)
+    expect(sumFuelRequirements('./day_01/input.txt', getFuel2)).toBe(4878818)
   })
 })
